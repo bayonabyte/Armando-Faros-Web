e2daa451a0b1b75bb69ba1ff989a29633e1de438
Jesús Bayona <j.bayona.2601@gmail.com>
2026-03-25 12:17:30 -0500

    test de db

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ app.permanent_session_lifetime = timedelta(days=6)
 
 def get_db():
     return mysql.connector.connect(
-        host=os.getenv('DB_HOST'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        database=os.getenv('DB_DATABASE'),
-        port=int(os.getenv('DB_PORT')),
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
 
 @app.route('/modelos')
